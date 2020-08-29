24076d5429185b3cf39c94287d727d88c5084928
Beatriz M. M. da Silva <beatriz.mm.silva@gmail.com>
2020-08-29 19:32:35 -0300

    Development (#3)
    * Adding tests
    
    * updating readme with tests
    
    * updating readme with spent time
    
    * Adding test for notifications already sent
    
    Co-authored-by: Bia <beatriz.silva@agriness.com>

--- README.md
+++ README.md
@@ -60,11 +60,11 @@
 
 * To scan the budgets and send the alerts:
 
-        docker run budgets_shop check`
+        docker run budgets_shop check
 
 * If you'd like to scan another dates too use "--searching-date":
 
-        docker run budgets_shop check --searching-date 2020-01-20
+        docker run budgets_shop check --searching-date 2020-06-20
 
 * To list the budgets:
 
@@ -78,12 +78,16 @@
 
         docker run budgets_shop --help
 
+### Testing:
+
+    pytest app/tests.py 
+
 ## Technologies:
 
 I've used SQLite as database. But, there's no need to import the sql.
-All the database setup is been doing at the apllication.
+All the database setup is been doing at the application.
 
-I've chose a ORM to deal with it. Peewee ORM is simple and minimalyst to work with smaller context.
+I've chosen an ORM to deal with it. Peewee ORM is simple and minimalist to work with smaller context.
 
 I'm using also a lib to make the CLI. Typer.
 
@@ -91,7 +95,7 @@ If you want to add data, you can write in app/migrations/data.py file, or in the
 
 I've add two more commands to visualize the data in the command line. The list of budgets and shops. That way, you can see what has changed in the database.
 
-Challenge resolution time: 5 hours.
+Challenge resolution time: 8 hours.
 
 You can run th application using python console or docker.
 
@@ -101,4 +105,4 @@ Yes, i'm checking if the shop is between 50 and 100 percent to make one notifica
 
 * How does your solution handle a budget change after a notification has already been sent?
 
-It checks all the stores, not only the online ones.
+It checks all the stores, not only the online ones. There's a unit test for it.
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def list_budgets():
     for budget in budgets:
         average_spent = (budget.amount_spent * 100) / budget.budget_amount
         typer.echo(str(budget.shop_id) + ' - ' + budget.shop_id.name)
+        typer.echo('Month: ' + str(budget.month))
         typer.echo('Budget amount: ' + str(budget.budget_amount))
         typer.echo('Amount spent: ' + str(budget.amount_spent))
         typer.echo('Average spent (%): ' + str(average_spent))
@@ -32,39 +33,41 @@ def list_shops():
 
 @application.command()
 def check(searching_date: str = str(datetime.today().date())):
-    ''' 
-        Command to scan the budgets and send the alerts...
+    ''' Command to scan the budgets and send the alerts...
         If you'd like to scan another dates too use "--searching-date":
 
-        Ex. python main.py check --searching-date 2020-01-20
-    '''
+        Ex. python main.py check --searching-date 2020-01-20'''
     date = datetime.strptime(searching_date, '%Y-%m-%d')
 
     budgets = Budgets.select().join(Shops, on=(Budgets.shop_id == Shops.id))\
         .where(
             (Budgets.month == date.replace(day=1).date())
         )
-
+    if not budgets:
+        typer.echo("No entries for this month")
     for budget in budgets:
         average_spent = (budget.amount_spent * 100) / budget.budget_amount
-        show_informations = False
+        shop = budget.shop_id
         if average_spent >= 100 and budget.shop_id.online:
-            show_informations = True
-            shop = budget.shop_id
             shop.online = False
             shop.save()
             typer.echo("you reached 100% of your budget, you've been blocked")
         elif 50 <= average_spent < 100:
-            show_informations = True
+            shop.online = True
+            shop.save()
             typer.echo('You reached 50% of the current months budget')
-        if show_informations:
-            remainder_budget = budget.budget_amount - budget.amount_spent
-            typer.echo(str(budget.shop_id) + ' - ' + budget.shop_id.name)
-            typer.echo('Date: ' + str(date.date()))
-            typer.echo("Month's Budget: " + str(budget.budget_amount))
-            typer.echo('Expenditure: ' + str(budget.amount_spent))
-            typer.echo('Budget remainder: ' + str(remainder_budget))
-            typer.echo('------------------')
+        elif average_spent < 50:
+            shop.online = True
+            shop.save()
+        else:
+            continue
+        remaining_budget = budget.budget_amount - budget.amount_spent
+        typer.echo(str(budget.shop_id) + ' - ' + budget.shop_id.name)
+        typer.echo('Date: ' + str(date.date()))
+        typer.echo("Month's Budget: " + str(budget.budget_amount))
+        typer.echo('Expenditure: ' + str(budget.amount_spent))
+        typer.echo('Remaining budget: ' + str(remaining_budget))
+        typer.echo('------------------')
 
 
 if __name__ == "__main__":
